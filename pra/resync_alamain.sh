7594b52dc1a74b1bbefa65536379810c81c6c8bd
Geoffroy Desvernay <dgeo@centrale-marseille.fr>
2022-10-13 16:05:21 +0200

    find last good snap on base too

--- pra/resync_alamain.sh
+++ pra/resync_alamain.sh
@@ -57,8 +57,15 @@ for fs in $(sed 's/@.*//' "$LISTSRC" | grep -v "${zfs}$" | sort -u); do
   fi
 done
 if [ $errcount -eq 0 ]; then
-  lastsrc=$(there zfs list -Honame -t snapshot -s creation "$zfs@$lastsrcsnap")
   lastdst=$(zfs list -Honame -t snapshot -s creation -r -d1 "$zfs" | tail -1)
+  there "zfs list -r -d1 -t snapshot -Honame $zfs" > "$LISTSRC"
+  if ! grep -q "^${lastdst}$" "$LISTSRC"; then
+    for snap in $(zfs list -Honame -t snapshot -s creation -r -d1 "$zfs" | fgrep $zfs); do
+      grep -q "^${snap}$" "$LISTSRC" && lastdst=${snap}
+    done
+    [ -n "$lastdst" ] && here zfs rollback "$lastdst"
+  fi
+  lastsrc=$(there zfs list -Honame -t snapshot -s creation "$zfs@$lastsrcsnap")
   # suppression des snapshots de synchro intermediaires inutiles
   there "zfs list -Honame -tsnapshot -r -d1 $zfs | grep '^$zfs@$snaphead' | egrep -v '($lastsrc|$lastdst|$lastvalidsnap)' | xargs -t -L1 zfs destroy -d"
   if [ "$lastsrc" != "$lastdst" ]; then
